362d3d2f42208b9d1ec878fc1e5d161eb09372df
Mat Sanders <BearFather@gmail.com>
2014-10-15 02:10:27 -0700

    .

--- MCirc/test/lib/chatout.php
+++
@@ -1,8 +0,0 @@
-<body text=white>
-</body>
-<?php
-//just loading up or class, nothing else here.  See irc.class.php for the good stuff.
-require('irc.class.php');
-$bot = new irc();
-$bot->entrar();
-?>
